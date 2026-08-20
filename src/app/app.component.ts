c25a4ee6594e14bcb7c1b55296d2a38273c6af97
Sanaka <bruna.lousilva@gmail.com>
2026-08-20 19:33:14 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'app-root',
+  styleUrl: './app.component.scss',
+  templateUrl: './app.component.html',
+})
+export class AppComponent {
+}
